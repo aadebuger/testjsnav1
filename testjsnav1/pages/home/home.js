db0f8cc937ed47f37acfa0657666c950e242776e
hua zhuang <zhvxxh@gmail.com>
2012-11-04 13:08:19 +0800

    first commit

---
+++ testjsnav1/pages/home/home.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // 每当用户导航至此页面时都要调用此功能。它
+        // 使用应用程序的数据填充页面元素。
+        ready: function (element, options) {
+            // TODO: 在此处初始化页面。
+            WinJS.Utilities.query("a").listen("click", linkClickEventHandler, false);
+        }
+    });
+
+    function linkClickEventHandler(eventInfo) {
+        eventInfo.preventDefault();
+        var link = eventInfo.target;
+        WinJS.Navigation.navigate(link.href);
+    }
+
+    
+})();
